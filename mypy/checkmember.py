e7765a2b385ce7474ab34afb2651b2844efceb95
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-12-24 22:37:57 +0100

    Fix 1

--- mypy/checkmember.py
+++ mypy/checkmember.py
@@ -400,15 +400,18 @@ def analyze_type_callable_member_access(name: str, typ: FunctionLike, mx: Member
                 ret_type, name, mx, original_vars=typ.items[0].variables, mcs_fallback=typ.fallback
             )
             if result:
-                if isinstance(result, Instance):
-                    return result
-                from mypy.expandtype import expand_type
+                if isinstance(result, CallableType):
+                    from mypy.expandtype import expand_type
 
-                env: dict[TypeVarId, TypeVarLikeType] = {t.id: t for t in result.variables}
-                env.update(
-                    {t.id: type_ for t, type_ in zip(ret_type.type.defn.type_vars, ret_type.args)}
-                )
-                return expand_type(result, env)
+                    env: dict[TypeVarId, TypeVarLikeType] = {t.id: t for t in result.variables}
+                    env.update(
+                        {
+                            t.id: type_
+                            for t, type_ in zip(ret_type.type.defn.type_vars, ret_type.args)
+                        }
+                    )
+                    return expand_type(result, env)
+                return result
         # Look up from the 'type' type.
         return _analyze_member_access(name, typ.fallback, mx)
     else:
